e0ae387c545bd19e0897b479705e8615b0aebe44
Ian Shi <72175372+lifeablestuff@users.noreply.github.com>
2022-10-27 12:59:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,20 +10,13 @@ class flash(Fl_Window):
 			self.but_list[-1].box(FL_FLAT_BOX)
 			self.but_list[-1].color(FL_GREEN)
 		self.flash = Fl_Button(160,180,90,40,'flash')
-		self.pause = Fl_Button(160,230,90,40,'pause')
-		self.pause.type(FL_SHADOW_BOX)
 		self.end()
 		self.flash.callback(self.flash_cb)
-		self.pause.callback(self.flash_cb)
-	
-	def pause_cb(self,wid):
-		Fl.remove_timeout(self.change_colour)
-		Fl.remove_timeout(self.change_back)
 	
 	def flash_cb(self,wid):
 		for x in range(len(self.but_list)):
-			Fl.add_timeout(1.0*x,self.change_colour,x)
-			Fl.add_timeout(0.5+x,self.change_back,x)
+			Fl.add_timeout(2.0+ 2.0*x,self.change_colour,x)
+			Fl.add_timeout(3.0+ 2.0*x,self.change_back,x)
 	
 	def change_colour(self,num):
 		self.but_list[num].color(FL_RED)
